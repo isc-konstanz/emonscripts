b235b50884ce751978134e8774aff4200e8bec92
Adrian Minde <Adrian_Minde@live.de>
2019-10-14 10:53:10 +0200

    Initialize isc-konstanz repository

--- install/init.sh
+++ install/init.sh
@@ -13,7 +13,7 @@ sudo chown $user $openenergymonitor_dir
 sudo mkdir $emoncms_dir
 sudo chown $user $emoncms_dir
 
-git clone -b isc https://github.com/openenergymonitor/EmonScripts.git $openenergymonitor_dir/EmonScripts
+git clone -b isc https://github.com/isc-konstanz/EmonScripts.git $openenergymonitor_dir/EmonScripts
 cd $openenergymonitor_dir/EmonScripts/install
 bash ./main.sh
 
